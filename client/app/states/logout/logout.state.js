e1feb7c04f528ad3b64bf515297324990aede25a
Romain GUILLOT <guilrom@gmail.com>
2019-07-03 17:49:56 +0200

    trying to fix uncomplete logout

--- client/app/states/logout/logout.state.js
+++ client/app/states/logout/logout.state.js
@@ -15,12 +15,13 @@ function getStates () {
 }
 
 /** @ngInject */
-function StateController (Session, $window) {
+function StateController (Session, API_BASE, $window) {
   activate()
 
   function activate () {
     var targetLocation
     const authType = Session.getAuthType()
+    console.log('authType: ', authType)
     Session.destroy()
     const location = $window.location.href
     if (location.includes(`/ui/service`)) {
@@ -29,7 +30,7 @@ function StateController (Session, $window) {
       targetLocation = `/`
     }
     if ('oidc' == authType) {
-      $window.location.href = '/oidc_login/redirect_uri?logout=' + encodeURI(targetLocation) 
+      $window.location.href = '/oidc_login/redirect_uri?logout=' + encodeURI(API_BASE + targetLocation) 
     } else {
       $window.location.href = targetLocation
     }
